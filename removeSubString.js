7bf150587c6cee8fcf235ad03f557fc004f7119c
Chris Athanas <realityexpanderdev@gmail.com>
2019-01-14 14:20:27 -0600

    Refactor matchCharIndex (no longer needed)

--- removeSubString.js
+++ removeSubString.js
@@ -13,40 +13,53 @@ function remove( matchString, fromText) {
   matchString = matchString.split("");
   fromText = fromText.split("");
 
-  let i = 0;
-  let matchChar = 0;
+  let i = 0; // index of current char in string
+  let matchCharIdx = 0; // index of current match in the matchString
   let foundMatch = false;
   let matchCount = 0;
-  let matchStart = 0;
+  let matchStart = 0; // start index of match in fromText string
 
-//   do {
+  let x = 0;
+
+  do {
+
     foundMatch = false;
-    i = 0;
-    matchIdx = 0;
+    i = 0; 
+    matchCount = 0;
+    matchStart = 0;
     
-    while( i < fromText.length && foundMatch == false) {
-      if(fromText[i] === matchString[i] ) {
+    while( i < fromText.length) {
+      if(fromText[i] === matchString[matchCount] ) {
         matchStart = matchCount === 0 ? i : matchStart;
         matchCount++;
+//         matchCharIdx++;
         if(matchCount >= matchString.length) {
           // we have a match
+         
+          fromText = fromText.slice(0, Math.max(0, matchStart-1)) 
+            .concat( fromText.slice(matchStart+matchCount, fromText.length+1) );
+          i = 0;
+          matchCount = 0;
+          matchStart = 0;
           foundMatch = true;
-          fromText = fromText.slice(0, Math.max(0,matchStart-1)) 
-            + fromText.slice(matchIdx+1, fromText.length);
         }
       } else {
-        matchIdx = i;
+//         matchCharIdx = 0;
         matchCount = 0;
+        matchStart = i;
       }
-    }
 
+      i++;
+    }
     
-//   } while (foundMatch === true);
+  } while (foundMatch === true || x++>100);
+
+  console.log(x);
 
   return fromText;
 }
 
 console.log("hello");
 var matchString = "ABC";
-var fromText = "ABCDEF";
+var fromText = "ABCDEFABC";
 console.log( remove(matchString, fromText) );
